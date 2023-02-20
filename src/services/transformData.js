713c758830e7bbfadd8e738932aabb6a6bfb29cd
Ryan <ryanlooney58@gmail.com>
2023-02-19 22:52:10 -0500

    refactoring external services

--- src/services/transformData.js
+++ src/services/transformData.js
@@ -1,11 +1,21 @@
 import fs from 'fs';
+import { group } from 'd3';
 
 // path to read data from
 const dataPath = "./assets/";
 
 let budgetMonths = fs.readFileSync(dataPath + 'budgetMonths.json', 'utf8', (err, data) => {if (err) throw err; console.log("budgetMonths read successfully.\n"); console.log(data)} );
-let transactionMonths = fs.readFileSync(dataPath + 'transactions.json', (err) => {if (err) throw err; console.log("transactions read successfully.\n")} );
+let transactions = fs.readFileSync(dataPath + 'transactions.json', 'utf8', (err) => {if (err) throw err; console.log("transactions read successfully.\n")} );
 
-// convert budget months to an array
-budgetMonths = budgetMonths.split("")
-console.log(budgetMonths)
+// convert JSON files back to Json type from String
+transactions = JSON.parse(transactions);
+budgetMonths = JSON.parse(budgetMonths);
+
+const groupTransactionsByCategory = (transactions) => {
+
+    console.log(transactions)
+    transactions = group(transactions, t=>t.month_year, t=>t.category_group_name);
+    console.log(transactions);
+
+}
+groupTransactionsByCategory(transactions);
