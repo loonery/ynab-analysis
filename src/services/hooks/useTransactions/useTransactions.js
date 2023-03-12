2c5329123052aa22add8f1d84b9dd394ad6b4fad
Ryan <ryanlooney58@gmail.com>
2023-03-12 11:36:23 -0400

    working on apollo hook for transactions

--- src/services/hooks/useTransactions/useTransactions.js
+++ src/services/hooks/useTransactions/useTransactions.js
@@ -1,88 +1,32 @@
 import { useQuery } from "@apollo/client";
-import { GET_TRANSACTIONS } from "../../queries/queries";
+import { GET_CATEGORY_GROUPS, GET_TRANSACTIONS } from "../../queries/queries";
+import { getFlattenedTransactions } from "./useTransactionHelper";
 
 export const useTransactions = () => {
     const budgetId = process.env.REACT_APP_BUDGET_ID;
+    const query = {loading: true, error: undefined, data: undefined}
     
-    const { loading, error, data: transactions } = useQuery(
+    // use the queries defined in the queries file to get the data 
+    const { loading: tLoading, error: tError, data: transactions } = useQuery(
         GET_TRANSACTIONS, 
         {variables: {
             budgetId: budgetId
         }}
     );
+
+    const { loading: cLoading, error: cError, data: categoryGroups } = useQuery(GET_CATEGORY_GROUPS);
+
+    if (!cLoading && tLoading) {
+        query.loading = false;
+    } 
+    else if (cError || tError) {
+        query.error = (cError ? cError : tError)
+    } 
     
-    // once we have the data we process it 
-    if (transactions) {
-        
-    // get category groups so that they can be matched with each child category in the flattened output
-    let transactions = data;
-    let categoryGroups = tr;
-
-    let i = 0;
-    while (i < transactions.length) {
-
-        // get the transaction object
-        let transaction = transactions[i];
-
-        // if the transaction object is a split transaction...
-        const subtransactions = transaction.subtransactions;
-        if (subtransactions && subtransactions.length > 0) {
-
-            // then process the subtransactions
-            let newSubtransactions = subtransactions.map((subtransaction) => {
-                
-                // modify each subtransaction to have the same fields as its parent transaction
-                const newTransaction = {...transaction, ...subtransaction};
-                newTransaction.category_group_name = getCategoryGroupBySubcategoryId(categoryGroups, newTransaction.category_id);                 
-                newTransaction.amount = newTransaction.amount / 1000;
-                
-                delete newTransaction.import_id
-                delete newTransaction.transaction_id;   // deletes the parent transaction id
-                delete newTransaction.subtransactions;
-                delete newTransaction.import_payee_name;
-                delete newTransaction.matched_transaction_id;
-                delete newTransaction.import_payee_name_original;
-                delete newTransaction.flag_color;
-
-                formatTransactionDate(newTransaction);
-                handleNullCategoryGroups(newTransaction);
-
-                // return the subtransaction mutated to hold the same JSON format as the parent transaction
-                return newTransaction;
-            })
-
-            // in the parent transaction's place, place its child transactions
-            const parentTransactionIndex = transactions.indexOf(transaction);
-            transactions.splice(parentTransactionIndex, 1, ...newSubtransactions);
-            i += newSubtransactions.length; 
-
-        } else {
-
-            // if there are no subtransactions, just mutate the transaction's data
-            transaction.category_group_name = getCategoryGroupBySubcategoryId(categoryGroups, transaction.category_id);
-            transaction.amount = transaction.amount / 1000;
-            
-            delete transaction.import_id
-            delete transaction.subtransactions;
-            delete transaction.import_payee_name
-            delete transaction.matched_transaction_id
-            delete transaction.import_payee_name_original
-
-            formatTransactionDate(transaction);
-            handleNullCategoryGroups(transaction);
-
-            // move to the next transaction
-            i += 1;
-        }
+    // if we have the data we can send it to be processed
+    else if (transactions && categoryGroups) {
+        query.data = getFlattenedTransactions(transactions, categoryGroups);
     }
 
-        
-        
-    }
-
-
-
-
-    return {loading, error, data}
-
+    return query;
 }
