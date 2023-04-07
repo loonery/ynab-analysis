5886583465b3a13aa267b411e09a42d55fddc2bd
Ryan <ryanlooney58@gmail.com>
2023-04-07 19:54:50 -0400

    editing file structure of the reuse library and simplifying scrollable container

---
+++ src/libs/reuse/styled/ScrollableListContainer.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+    
+// defines the container that holds the scrollable content
+export const ScrollableContentContainer = styled.div`
+    max-height: 200px;
+    width: max-content;
+    overflow: auto;
+    padding: 6px;
+`
