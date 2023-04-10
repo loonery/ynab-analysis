94b0d80fb19ea5a3a5806b4b7372d8f2d771059a
Ryan <ryanlooney58@gmail.com>
2023-04-10 07:51:05 -0400

    moving button definitions out of dropdown

--- src/components/FilterBar/CategoryFilterDropdown/CategoryFilterDropdown.js
+++ src/components/FilterBar/CategoryFilterDropdown/CategoryFilterDropdown.js
@@ -1,32 +1,9 @@
 import { Dropdown } from 'libs/reuse/composite/Dropdown';
-import styled from 'styled-components';
 import CategoryCheckBoxesContainer from './CategoryCheckBoxesContainer'
 import ButtonBar from 'libs/reuse/composite/ButtonBar';
+import { HeaderButtons, FooterButtons } from './DropdownButtonConfig';
 
 const CategoryFilterDropdown = () => {
-
-    // header button definitions
-    const HeaderButtons = [
-        {
-            label: "Select None",
-            onClick: () => {},
-            classString: 'btn btn-sm btn-outline-dark'
-        },
-        {
-            label: "Select All",
-            onClick: () => {},
-            classString: 'btn btn-sm btn-outline-primary'
-        },
-    ];
-
-    // footer button definitions
-    const FooterButtons = [
-        {
-            label: "Done",
-            onClick: () => {},
-            classString: 'btn btn-sm btn-success'
-        }
-    ];
 
     return (
         <Dropdown dropdownLinkText={"Categories"}>
---
+++ src/components/FilterBar/CategoryFilterDropdown/DropdownButtonConfig.js
@@ -0,0 +1,22 @@
+// header button definitions
+export const HeaderButtons = [
+    {
+        label: "Select None",
+        onClick: () => {},
+        classString: 'btn btn-sm btn-outline-dark'
+    },
+    {
+        label: "Select All",
+        onClick: () => {},
+        classString: 'btn btn-sm btn-outline-primary'
+    },
+];
+
+// footer button definitions
+export const FooterButtons = [
+    {
+        label: "Done",
+        onClick: () => {},
+        classString: 'btn btn-sm btn-success'
+    }
+];
